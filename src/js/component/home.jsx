83acd58dd7030fe185399dc6da0c7b3d9118c35f
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-07-26 17:45:01 +0000

    fixed API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 const TodoList = () => {
-  const [inputValue, setInputValue] = useState(""); // State for the input value
-  const [list, setList] = useState([]); // State for the list of tasks
+  const [inputValue, setInputValue] = useState("");
+  const [list, setList] = useState([]);
 
-  useEffect(() => { // Fetch the tasks from the server
-    fetchTasks(); 
+  useEffect(() => {
+    fetchTasks();
   }, []);
- 
+
   const fetchTasks = () => {
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr")
-      .then((response) => response.json()) // Convert the response to JSON
-      .then((data) => setList(data)) // Set the list with the data from the server
-      .catch((error) => console.log(error)); // Catch any errors
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setList(data))
+      .catch((error) => console.log(error));
+  };
+
+  const addTask = (task) => {
+    const updatedTodos = [...list, task];
+    setList(updatedTodos);
+    syncTasksWithServer(updatedTodos, "POST");
   };
 
   const syncTasksWithServer = (updatedTodos, method) => {
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
-      method: method, // a type of method
-      body: JSON.stringify(updatedTodos), 
+    fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: method,
+      body: JSON.stringify(updatedTodos),
       headers: {
         "Content-Type": "application/json",
       },
@@ -27,37 +33,31 @@ const TodoList = () => {
       .then((data) => console.log(data))
       .catch((error) => console.log(error));
   };
-  // update on the frontend and synchronize with the server. 
-  const Input = (task) => { 
-    const updatedTodos = [...list, task]; // Add the new task to the list
-    setList(updatedTodos); // Update the list
-    syncTasksWithServer(updatedTodos, "PUT"); // Update the server with method PUT
-  };
-  // Function to add a new task from user input
-  const AddTask = () => { 
-    if (inputValue.trim() !== "") { // Check if the input value is not empty
-      const newTask = { // Create a new task
-        label: inputValue, // Set the label of the task
-        done: false, // Set the task as not done
+
+  const addTaskHandler = () => {
+    if (inputValue.trim() !== "") {
+      const newTask = {
+        title: inputValue,
+        completed: false,
       };
-      Input(newTask); // Add the new task to the list
-      setInputValue(""); // Reset the input value
+      addTask(newTask);
+      setInputValue("");
     }
   };
 
-  const EnterKey = (event) => { // Function to add a new task by pressing Enter key
+  const handleEnterKey = (event) => {
     if (event.key === "Enter") {
-      AddTask();
+      addTaskHandler();
     }
   };
-  // delete a task from the list
-  const DeleteTheTask = (taskIndex) => {
-    const updatedTodos = list.filter((_, index) => index !== taskIndex);
+
+  const deleteTaskHandler = (taskId) => {
+    const updatedTodos = list.filter((task) => task.id !== taskId);
     setList(updatedTodos);
     syncTasksWithServer(updatedTodos, "DELETE");
   };
-  // delete all tasks from the list
-  const DeleteAllTasks = () => {
+
+  const deleteAllTasksHandler = () => {
     const updatedTodos = [];
     setList(updatedTodos);
     syncTasksWithServer(updatedTodos, "DELETE");
@@ -77,27 +77,21 @@ const TodoList = () => {
                   value={inputValue}
                   onChange={(e) => setInputValue(e.target.value)}
                   placeholder="Enter new task..."
-                  onKeyDown={EnterKey}
+                  onKeyDown={handleEnterKey}
                 />
               </div>
-              <button
-                onClick={AddTask}
-                className="btn btn-sm btn-primary"
-              >
+              <button onClick={addTaskHandler} className="btn btn-sm btn-primary">
                 Add Task
               </button>
-              <button
-                onClick={DeleteAllTasks}
-                className="btn btn-sm btn-danger ml-2"
-              >
+              <button onClick={deleteAllTasksHandler} className="btn btn-sm btn-danger ml-2">
                 Delete All Tasks
               </button>
               <ul className="list-group mt-3">
-                {list.map((task, index) => (
-                  <li key={index} className="list-group-item">
-                    {task.label}
+                {list.map((task) => (
+                  <li key={task.id} className="list-group-item">
+                    {task.title}
                     <button
-                      onClick={() => DeleteTheTask(index)}
+                      onClick={() => deleteTaskHandler(task.id)}
                       className="btn btn-sm btn-danger float-right"
                     >
                       Delete
