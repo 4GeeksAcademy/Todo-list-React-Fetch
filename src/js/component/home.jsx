732e430f52feb44319f7730bcc9146a711192528
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-07-26 20:47:49 +0000

    added comments and fixed delete task

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,51 +1,35 @@
 import React, { useState, useEffect } from "react";
 
-const TodoList = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [list, setList] = useState([]);
-
-  useEffect(() => {
-    fetchTasks();
+const TodoList = () => { 
+  const [inputValue, setInputValue] = useState(""); // input value
+  const [list, setList] = useState([]); // list of tasks
+ 
+  useEffect(() => { 
+    fetchTasks(); // fetch tasks from server
   }, []);
 
   const fetchTasks = () => {
     fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((data) => setList(data))
-      .catch((error) => console.log(error));
+      .then((response) => response.json()) // convert response to json
+      .then((data) => setList(data)) // set list with data from server
+      .catch((error) => console.log(error)); // catch errors
   };
-
-  const addTask = (task) => {
-    const updatedTodos = [...list, task];
-    setList(updatedTodos);
-    syncTasksWithServer(updatedTodos, "POST");
-  };
-
-  const syncTasksWithServer = (updatedTodos, method) => {
-    fetch("https://jsonplaceholder.typicode.com/todos", {
-      method: method,
-      body: JSON.stringify(updatedTodos),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  };
-
+  // addTaskHandler is called when the user clicks on the Add Task button
   const addTaskHandler = () => {
-    if (inputValue.trim() !== "") {
-      const newTask = {
-        title: inputValue,
-        completed: false,
+    if (inputValue.trim() !== "") { // check if input value is not empty
+      const newTask = { // create new task object
+        id: Date.now(), // generate unique id
+        title: inputValue, // get input value
+        completed: false, // set completed to false
       };
-      addTask(newTask);
-      setInputValue("");
+      const updatedTodos = [...list, newTask]; // add new task to list
+      setList(updatedTodos); // update list
+      setInputValue(""); // clear input value
+      syncTasksWithServer(newTask, "POST"); // use POST method to add new task to server
     }
   };
-
-  const handleEnterKey = (event) => {
+  // use ENTET key to add new task
+    const handleEnterKey = (event) => {
     if (event.key === "Enter") {
       addTaskHandler();
     }
@@ -54,13 +38,24 @@ const TodoList = () => {
   const deleteTaskHandler = (taskId) => {
     const updatedTodos = list.filter((task) => task.id !== taskId);
     setList(updatedTodos);
-    syncTasksWithServer(updatedTodos, "DELETE");
+    syncTasksWithServer(taskId, "DELETE");
   };
 
   const deleteAllTasksHandler = () => {
-    const updatedTodos = [];
-    setList(updatedTodos);
-    syncTasksWithServer(updatedTodos, "DELETE");
+    setList([]);
+    syncTasksWithServer(list.map((task) => task.id), "DELETE");
+  };
+
+  const syncTasksWithServer = (taskId, method) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${taskId}`, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
   };
 
   return (
